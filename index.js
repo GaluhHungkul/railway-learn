3bfd14dbc16be58e0c353410cbb08827fe76d2e4
GaluhHungkul <muhamadgaluh31@gmail.com>
2025-05-26 07:34:35 +0700

    setup prisma

---
+++ index.js
@@ -0,0 +1,28 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const PORT = 3001
+const prisma = require("./lib/prisma")
+const cors = require("cors")
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended : true }))
+
+app.get("/", async (req,res) => {
+    const user = await prisma.user.findMany()
+    res.json(user)
+})
+
+app.post("/user",  async (req,res) => {
+    console.log(req.body)
+    const { name, email } = req.body
+    const newUser = await prisma.user.create({
+        data : { name, email }
+    })
+    res.json(newUser)
+})
+
+app.listen(PORT, () => {
+    console.log(`Listen to http://localhost:${PORT}`)
+})
